0ba18a32e5a3f43ffed82cbe61c9b3219e68536d
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-06-24 12:20:37 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ // quais os arquivos onde se utiliza o tailwind
+		'./src/**/*.tsx'
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
