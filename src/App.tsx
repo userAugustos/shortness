e418d5c6ef32e2d8a8b6b2c30eca7a2f41533f9f
Felipe Augustos <felipe_augustos84@outlook.com.br>
2022-06-26 16:45:35 -0300

    chore: testing connection with graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useQuery } from '@apollo/client' // this a hook from apollo integration with react, facilitates the react call for graphql
+import { GET_LESSONS_QUERY } from './utils/querys'
+import { ILesson } from './utils/types/lessons'
 
 function App() {
+	const { data } = useQuery<{ lessons: ILesson[] }>(GET_LESSONS_QUERY) // example of a query
+	// above i'm passing directly the type of return of query, an object with and array of lessons
+
+	console.log(data?.lessons)
+
 	return (
-		<h1 className='text-violet-400'>
-			Hello World
-		</h1>
+		<ul>
+			{data?.lessons.map(lesson =>(
+				<li key={lesson.id}>{lesson.title}</li>
+			))}
+		</ul>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { client } from './lib/apollo'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}> {/* ApolloProvider is a context provider, and in this context, we provider the conection (client) */}
+			<App />
+		</ApolloProvider>
   </React.StrictMode>
 )
